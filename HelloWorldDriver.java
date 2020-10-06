55bc417cd789426a878f2f5d3de08dcb6a02e47d
Rweidenh <54329093+Rweidenh@users.noreply.github.com>
2020-10-06 19:09:40 -0400

    test commit

---
+++ HelloWorldDriver.java
@@ -0,0 +1,23 @@
+import java.sql.*;
+
+public class HelloWorldDriver {
+
+	public static void main(String[] args) {
+	try{  
+		Class.forName("com.mysql.jdbc.Driver");  
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3308/ap","root","");  
+		Statement stmt=con.createStatement();  
+		ResultSet rs=stmt.executeQuery("select * from sampletable");  
+		while(rs.next())  {
+		System.out.println("Student ID is: "+rs.getInt(1)+"\nName is: "+rs.getString(2)+"\nMajor is: "+rs.getString(3));  
+		}
+		con.close();  }
+
+			catch(Exception e)
+			{ 
+				System.out.println(e);
+				//d
+			}   
+		}
+		
+	}
